96b504f7bb52cfca9d35d141dfed226ec5af2a78
JadePaola2 <jade66219@gmail.com>
2023-11-14 16:33:05 -0500

    descarlalaap y negocio-info

--- js/negocio.js
+++ js/negocio.js
@@ -1,41 +1,51 @@
 let parrafoTex_btn1 = document.getElementById("parrafoTex_btn1");
-let parrafoTex1 = document.getElementById("parrafoTex1");
+let parrafoTex1 = document.getElementById("parrafoTex1Contenido");
+let arrow1 = document.querySelector("#parrafoTex_btn1 .fa-arrow-right");
 
 parrafoTex_btn1.addEventListener("click", toggleText1);
 function toggleText1() {
     parrafoTex1.classList.toggle("show");
+    arrow1.classList.toggle("rotate");
 }
 
 let parrafoTex_btn2 = document.getElementById("parrafoTex_btn2");
-let parrafoTex2 = document.getElementById("parrafoTex2");
+let parrafoTex2 = document.getElementById("parrafoTex2Contenido");
+let arrow2 = document.querySelector("#parrafoTex_btn2 .fa-arrow-right");
 
 parrafoTex_btn2.addEventListener("click", toggleText2);
 function toggleText2() {
     parrafoTex2.classList.toggle("show");
+    arrow2.classList.toggle("rotate");
 }
 
 let parrafoTex_btn3 = document.getElementById("parrafoTex_btn3");
-let parrafoTex3 = document.getElementById("parrafoTex3");
+let parrafoTex3 = document.getElementById("parrafoTex3Contenido");
+let arrow3 = document.querySelector("#parrafoTex_btn3 .fa-arrow-right");
 
 parrafoTex_btn3.addEventListener("click", toggleText3);
 function toggleText3() {
     parrafoTex3.classList.toggle("show");
+    arrow3.classList.toggle("rotate");
 }
 
 let parrafoTex_btn4 = document.getElementById("parrafoTex_btn4");
-let parrafoTex4 = document.getElementById("parrafoTex4");
+let parrafoTex4 = document.getElementById("parrafoTex4Contenido");
+let arrow4 = document.querySelector("#parrafoTex_btn4 .fa-arrow-right");
 
 parrafoTex_btn4.addEventListener("click", toggleText4);
 function toggleText4() {
     parrafoTex4.classList.toggle("show");
+    arrow4.classList.toggle("rotate");
 }
 
 let parrafoTex_btn5 = document.getElementById("parrafoTex_btn5");
-let parrafoTex5 = document.getElementById("parrafoTex5");
+let parrafoTex5 = document.getElementById("parrafoTex5Contenido");
+let arrow5 = document.querySelector("#parrafoTex_btn5 .fa-arrow-right");
 
 parrafoTex_btn5.addEventListener("click", toggleText5);
 function toggleText5() {
     parrafoTex5.classList.toggle("show");
+    arrow5.classList.toggle("rotate");
 }
 
 
